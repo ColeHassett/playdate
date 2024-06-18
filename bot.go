dc3de469c8144afe8433cc5fa2d3e28748684ce2
ColeHassett <36613666+ColeHassett@users.noreply.github.com>
2024-06-17 23:02:17 -0400

    IT WORKS NOW IF YOU HAVE A KEY (#3)

--- bot.go
+++ bot.go
@@ -1,10 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
-// import "github.com/bwmarrin/discordgo"
-// import "github.com/go-co-op/gocron/v2"
+	"github.com/bwmarrin/discordgo"
+	// "github.com/go-co-op/gocron/v2"
+)
+
+var (
+	BotToken = flag.String("token", "", "Bot Token")
+)
+
+func init() { flag.Parse() }
 
 func main() {
-	fmt.Print("arst")
+
+	if *BotToken == "" {
+		fmt.Println("Not token specified")
+		return
+	}
+
+	dg, err := discordgo.New("Bot " + *BotToken)
+	if err != nil {
+		fmt.Println("YOU RUINED IT: ", err)
+		return
+	}
+
+	dg.AddHandler(func(s *discordgo.Session, event *discordgo.Ready) {
+		s.ChannelMessageSend("522472817283956745", "FARTS WE GO IN")
+	})
+	dg.AddHandler(messageCreate)
+
+	// Receive message events
+	// THIS IS DOGSHIT
+	dg.Identify.Intents = discordgo.IntentsGuilds | discordgo.IntentsGuildMessages
+
+	// Open websocket connection to discord
+	dg.Open()
+
+	// Wait for ctrl+c to close app
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+	dg.Close()
+
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Content == "hi" {
+		s.ChannelMessageSend("522472817283956745", "Hello World")
+	}
+	fmt.Println("MESSAGE: ", m.Content)
+	fmt.Println("AUTHOR: ", m.Author.ID)
+	fmt.Println("CHANNEL: ", m.ChannelID)
+	fmt.Println("GUILD?: ", m.GuildID)
 }
