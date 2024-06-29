84d2e28747ff290feb86c035fbf8d6e6fbad0edf
ColeHassett <colehassett@gmail.com>
2024-06-29 11:15:03 -0400

    initial group getters

--- bot.go
+++ bot.go
@@ -3,8 +3,12 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
@@ -46,6 +50,8 @@ func main() {
 		return
 	}
 
+	startAPI()
+
 	// Wait for ctrl+c to close app
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
@@ -55,11 +61,56 @@ func main() {
 }
 
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	if m.Content == "hi" {
-		s.ChannelMessageSend("522472817283956745", "Hello World")
-	}
 	fmt.Println("MESSAGE: ", m.Content)
 	fmt.Println("AUTHOR: ", m.Author.ID)
 	fmt.Println("CHANNEL: ", m.ChannelID)
 	fmt.Println("GUILD?: ", m.GuildID)
+
+	if strings.HasPrefix(m.Content, "!groups") {
+		c, err := s.State.Channel(m.ChannelID)
+		if err != nil {
+			return
+		}
+
+		resp, err := http.Get("http://localhost:8080/groups")
+		if err != nil {
+			log.Fatalln(err)
+			return
+		}
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatalln(err)
+			return
+		}
+
+		groups := string(body)
+
+		s.ChannelMessageSend(c.ID, groups)
+
+	} else if strings.HasPrefix(m.Content, "!group") {
+		c, err := s.State.Channel(m.ChannelID)
+		if err != nil {
+			return
+		}
+
+		params := strings.Fields(m.Content)
+		id := params[1]
+
+		resp, err := http.Get("http://localhost:8080/group/" + id)
+		if err != nil {
+			log.Fatalln(err)
+			return
+		}
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatalln(err)
+			return
+		}
+
+		group := string(body)
+
+		s.ChannelMessageSend(c.ID, group)
+	}
 }
