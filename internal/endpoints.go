d90e4bf57c6adbb66efa9b1ea5a76950c61ce3a0
Colin Thatcher <colinthatcher0@gmail.com>
2025-06-07 21:46:34 -0400

    Added additional logic to handle errors on all pages, allow users to signup to a playdate, introduced zerolog

--- internal/endpoints.go
+++ internal/endpoints.go
@@ -4,57 +4,52 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 	"strconv"
-	"text/template"
+	"strings"
 	"time"
 
+	"github.com/gin-contrib/logger"
 	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"github.com/uptrace/bun"
 )
 
 func StartAPI(db *bun.DB) {
 	api := Api{db: db, ctx: context.Background()}
 
-	router := gin.Default()
+	router := gin.New()        // NOTE: Not using Default to avoid the wrong logger being used?
+	router.Use(gin.Recovery()) // handle panics (aka unhandled exceptions)
+	gin.SetMode(gin.DebugMode) // adds additional debugging features for the gin http server.
+
+	// Add the zerolog to gin's middleware
+	router.Use(logger.SetLogger(
+		logger.WithUTC(true),                            // Log time in UTC
+		logger.WithDefaultLevel(zerolog.InfoLevel),      // Default log level
+		logger.WithClientErrorLevel(zerolog.WarnLevel),  // Level for 4xx errors
+		logger.WithServerErrorLevel(zerolog.ErrorLevel), // Level for 5xx errors
+	))
 
 	// Template Endpoints
-	router.LoadHTMLGlob(fmt.Sprintf("%s/*", Config.TemplateDirectory))
+	router.LoadHTMLGlob(fmt.Sprintf("%s/**/*.html", Config.TemplateDirectory))
 	router.StaticFile("custom-colors.css", fmt.Sprintf("%s/custom-colors.css", Config.TemplateDirectory))
 	// Templated endpoints
 	router.GET("/", api.index)
-	router.GET("/home", api.home)
 	router.POST("/register", api.registerUserTemplate)
 	router.GET("/playdate", api.showPlayDateForm)
 	router.POST("/playdate", api.createPlayDateTemplate)
-
-	// API Endpoints
-	v1 := router.Group("/api/v1")
-	// /player
-	v1.GET("/player", api.getPlayers)
-	v1.POST("/player", api.createPlayer)
-	v1.GET("/player/:id", api.getPlayer)
-	v1.PATCH("/player/:id", api.updatePlayer)
-	v1.DELETE("/player/:id", api.deletePlayer)
-	// /playdate
-	v1.GET("/playdate", api.getPlayDates)
-	v1.POST("/playdate", api.createPlayDate)
-	v1.GET("/playdate/:id", api.getPlayDate)
-	v1.PATCH("/playdate/:id", api.updatePlayDate)
-	v1.DELETE("/playdate/:id", api.deletePlayDate)
+	router.GET("/playdate/:id", api.getPlayDateTemplate)
+	router.POST("/playdate/:id/yes", api.setPlayDateAttendence)
+	router.POST("/playdate/:id/maybe", api.setPlayDateAttendence)
+	router.POST("/playdate/:id/no", api.setPlayDateAttendence)
 
 	router.Run()
 }
 
-func renderPartialHtml(c *gin.Context, filepath string, state any) {
-	tmpl := template.Must(template.ParseFiles(fmt.Sprintf("%s/%s", Config.TemplateDirectory, filepath)))
-	err := tmpl.ExecuteTemplate(c.Writer, filepath, state)
-	if err != nil {
-		fmt.Printf("Error rendering %s: %v\n", filepath, err)
-		c.Status(http.StatusInternalServerError)
-		return
-	}
+type BaseTemplateData struct {
+	ServerError string
+	Errors      map[string]string
 }
 
 type Api struct {
@@ -64,109 +59,196 @@ type Api struct {
 
 func (a *Api) index(c *gin.Context) {
 	cookie, _ := c.Cookie("playdate")
-	type IndexState struct {
-		SignedUp  bool
-		PlayDates []*PlayDate
-	}
-	state := &IndexState{}
-	if cookie == "" {
-		c.HTML(http.StatusOK, "index.html", state)
-		return
-	}
-	// extra processing
-	state.SignedUp = true
-	state.PlayDates = []*PlayDate{}
-	err := a.db.NewSelect().Model(&state.PlayDates).Scan(a.ctx)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-	log.Println(state)
+	log.Debug().Str("Cookie", cookie).Str("HX-Request", c.Request.Header.Get("HX-Request")).Msg("rendering index")
 
-	c.HTML(http.StatusOK, "index.html", state)
+	state := gin.H{"Errors": map[string]string{}}
+	if cookie == "" {
+		c.HTML(http.StatusOK, "pages/register.html", gin.H{})
+		return
+	}
+
+	// render actual home page since user has a cookie
+	state["SignedUp"] = true
+	playdates := []*PlayDate{}
+	err := a.db.NewSelect().Model(&playdates).Scan(a.ctx)
+	if err != nil {
+		log.Error().Err(err).Msg("DB error in index")
+		state["ServerError"] = "Failed to retrieve playdates due to a server error. Please try again later."
+	}
+	state["PlayDates"] = playdates
+	c.HTML(http.StatusOK, "pages/home.html", state)
 }
 
 func (a *Api) showPlayDateForm(c *gin.Context) {
-	c.HTML(http.StatusOK, "playdate-form.html", gin.H{})
-}
-
-func (a *Api) home(c *gin.Context) {
-	type HomeState struct {
-		Playdates []PlayDate
-	}
-
-	state := &HomeState{Playdates: []PlayDate{}}
-	err := a.db.NewSelect().Model(&state.Playdates).Scan(a.ctx)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-
-	renderPartialHtml(c, "home.html", state)
+	c.HTML(http.StatusOK, "partials/playdate-form.html", gin.H{})
 }
 
 func (a *Api) createPlayDateTemplate(c *gin.Context) {
 	inputGame := c.PostForm("game")
 	inputDatetime := c.PostForm("date")
 
-	type FormData struct {
-		Game   string
-		Date   string
-		Errors map[string]string
-	}
-	formData := &FormData{Game: inputGame, Date: inputDatetime, Errors: make(map[string]string)}
+	formData := gin.H{"Game": inputGame, "Date": inputDatetime}
+	errors := map[string]string{}
 	if inputGame == "" {
-		formData.Errors["game"] = "game is required"
+		errors["game"] = "game is required"
 	}
 	if inputDatetime == "" {
-		formData.Errors["date"] = "date is required"
+		errors["date"] = "date is required"
 	}
 	// NOTE: that this date is not random but instead hardcoded into the standard
 	// libary to code layouts against.
 	expectedTimeLayout := "2006-01-02T15:04"
 	parsedDatetime, err := time.Parse(expectedTimeLayout, inputDatetime)
 	if err != nil {
-		formData.Errors["date"] = "invalid format for date/time, please use layout 2025-01-01T12:00"
+		errors["date"] = "invalid format for date/time, please use layout 2025-01-01T12:00"
 	}
-	if len(formData.Errors) > 0 {
-		renderPartialHtml(c, "playdate-form.html", formData)
+	formData["Errors"] = errors
+	if len(errors) > 0 {
+		c.HTML(http.StatusOK, "partials/playdate-form.html", formData)
 		return
 	}
 
 	playdate := PlayDate{Game: inputGame, Date: parsedDatetime}
 	_, err = a.db.NewInsert().Model(&playdate).Exec(a.ctx)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
+		log.Err(err).Any("playdate", playdate).Msg("failed to insert new playdate")
+		formData["ServerError"] = err
+		c.HTML(http.StatusOK, "partials/playdate-form.html", formData)
 		return
 	}
 
+	// redirect the user back to the index router (i.e. the homepage)
 	c.Header("HX-Location", "/")
+}
+
+func (a *Api) getPlayDateTemplate(c *gin.Context) {
+	state := gin.H{}
+	errors := map[string]string{}
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		// redirect the user to the home page if they request with an improper id
+		log.Err(err).Str("playdateID", c.Param("id")).Msg("failed to parse given playdate id")
+		c.Redirect(http.StatusFound, "/")
+		return
+	}
+
+	log.Info().Int("id", id).Msg("Querying for players related to playdate")
+	playdate := &PlayDate{ID: id}
+	err = a.db.NewSelect().Model(playdate).WherePK().Scan(c.Request.Context())
+	if err != nil {
+		// if the given id doesn't exist just return the called to the home page
+		log.Err(err).Int("playdateID", id).Msg("failed to find playdate")
+		c.Redirect(http.StatusFound, "/")
+		return
+	}
+	playdatePlayers := []*PlayDateToPlayer{}
+	err = a.db.NewSelect().Model(&playdatePlayers).Relation("Player").Where("id = ?", id).Scan(c.Request.Context())
+	if err != nil {
+		// report error back to user, but just render the page like normal
+		log.Err(err).Any("playdate", playdate).Msg("failed to find related players to playdate")
+		errors["PlayDatePlayers"] = err.Error()
+	}
+
+	log.Debug().Interface("playdatePlayers", playdatePlayers).Msg("Playdate players details")
+	state["Errors"] = errors
+	state["PlayDate"] = playdate
+	state["PlayDatePlayers"] = playdatePlayers
+	log.Debug().Interface("playdate", playdate).Msg("Playdate details")
+	if c.Request.Header.Get("HX-Request") == "" {
+		c.HTML(http.StatusOK, "pages/playdate.html", state)
+	} else {
+		c.HTML(http.StatusOK, "partials/playdate.html", state)
+	}
+}
+
+func (a *Api) setPlayDateAttendence(c *gin.Context) {
+	uriParts := strings.Split(c.Request.RequestURI, "/")
+	action := uriParts[len(uriParts)-1]
+	log.Debug().Str("action", action).Msg("received attendance action")
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		// redirect the user to the home page if they request with an improper id
+		log.Err(err).Str("playdateID", c.Param("id")).Msg("failed to parse given playdate id")
+		c.Redirect(http.StatusFound, "/")
+		return
+	}
+
+	playdate := &PlayDate{ID: id}
+	err = a.db.NewSelect().Model(playdate).WherePK().Scan(c.Request.Context())
+	if err != nil {
+		// if the given id doesn't exist just return the called to the home page
+		log.Err(err).Int("playdateID", id).Msg("failed to find playdate")
+		c.Redirect(http.StatusFound, "/")
+		return
+	}
+
+	player := &Player{}
+	cookie, err := c.Cookie("playdate")
+	if err != nil {
+		// redirect the user to the home page if they don't have a cookie
+		log.Err(err).Msg("failed to parse user's cookie")
+		c.Redirect(http.StatusFound, "/")
+		return
+	}
+
+	err = json.Unmarshal([]byte(cookie), player)
+	if err != nil {
+		// if the given player id doesn't exist just return the called to the home page
+		log.Err(err).Str("cookie", cookie).Int("playerID", id).Msg("failed to unmarshal cookie into player struct")
+		c.Redirect(http.StatusFound, "/")
+		return
+	}
+	err = a.db.NewSelect().Model(player).WherePK().Scan(c.Request.Context())
+	if err != nil {
+		// if the given player id doesn't exist just return the called to the home page
+		log.Err(err).Any("player", player).Int("playerID", id).Msg("failed to find the player from their cookie")
+		c.Redirect(http.StatusFound, "/")
+		return
+	}
+
+	log.Info().Int("playdateID", playdate.ID).Int("playerID", player.ID).Str("action", action).Msg("attempting to set playdate attendance")
+	errors := map[string]string{}
+	rel := &PlayDateToPlayer{PlayDateID: playdate.ID, PlayerID: player.ID, Attending: action}
+	_, err = a.db.NewInsert().Model(rel).On("CONFLICT (playdate_id, player_id) DO UPDATE").Set("attending = EXCLUDED.attending").Exec(a.ctx)
+	if err != nil {
+		// send error back to user within the players-table.html
+		log.Error().Err(err).Interface("relation", rel).Msg("failed to insert playdate to player relation")
+		errors["PlayDatePlayers"] = err.Error()
+	} else {
+		log.Info().Interface("relation", rel).Msg("successfully inserted playdate to player relation")
+	}
+
+	playdatePlayers := []*PlayDateToPlayer{}
+	err = a.db.NewSelect().Model(&playdatePlayers).Relation("Player").Where("id = ?", player.ID).Scan(c.Request.Context())
+	if err != nil {
+		// report error back to user, but just render the page like normal
+		log.Err(err).Any("playdate", playdate).Msg("failed to find related players to playdate")
+		errors["PlayDatePlayers"] = err.Error()
+	}
+
+	state := gin.H{}
+	state["Errors"] = errors
+	state["PlayDatePlayers"] = playdatePlayers
+	state["PlayDate"] = playdate
+	c.HTML(http.StatusOK, "partials/players-table.html", state)
 }
 
 func (a *Api) registerUserTemplate(c *gin.Context) {
 	name := c.PostForm("name")
 	discID := c.PostForm("discID")
 
-	type FormData struct {
-		Name   string
-		DiscID string
-		Errors map[string]string
-	}
-	formData := &FormData{Name: name, DiscID: discID, Errors: make(map[string]string)}
+	formData := gin.H{"Name": name, "DiscID": discID}
+	errors := map[string]string{}
 	if name == "" {
-		formData.Errors["name"] = "name is required"
+		errors["name"] = "name is required"
 	}
 	if discID == "" {
-		formData.Errors["discID"] = "discID is required"
+		errors["discID"] = "discID is required"
 	}
-	if len(formData.Errors) > 0 {
-		renderPartialHtml(c, "register.html", formData)
+	if len(errors) > 0 {
+		formData["Errors"] = errors
+		log.Debug().Any("errors", errors).Msg("form data didn't meet validation")
+		c.HTML(http.StatusOK, "partials/register.html", formData)
 		return
 	}
 
@@ -175,9 +257,9 @@ func (a *Api) registerUserTemplate(c *gin.Context) {
 	if err != nil {
 		_, err := a.db.NewInsert().Model(&player).Exec(a.ctx)
 		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"error": err.Error(),
-			})
+			log.Err(err).Msg("failed to create new player")
+			formData["ServerError"] = err.Error()
+			c.HTML(http.StatusOK, "partials/register.html", formData)
 			return
 		}
 	}
@@ -186,212 +268,4 @@ func (a *Api) registerUserTemplate(c *gin.Context) {
 	c.SetCookie("playdate", string(val), 0, "/", "", false, true)
 	c.Status(http.StatusCreated)
 	c.Header("HX-Location", "/")
-	// renderPartialHtml(c, "home.html", nil)
-}
-
-func (a *Api) getPlayers(c *gin.Context) {
-	var players []Player
-	err := a.db.NewSelect().Model(&players).Scan(a.ctx)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-	c.JSON(http.StatusOK, players)
-}
-
-func (a *Api) createPlayer(c *gin.Context) {
-	var json Player
-	err := c.ShouldBindJSON(&json)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-	_, err = a.db.NewInsert().Model(&json).Exec(a.ctx)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-	c.JSON(http.StatusCreated, gin.H{})
-}
-
-func (a *Api) getPlayer(c *gin.Context) {
-	id := c.Param("id")
-	if id == "" {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "missing required parameter id"})
-		return
-	}
-	var player Player
-	err := a.db.NewSelect().Model(&player).Scan(a.ctx)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-	c.JSON(http.StatusOK, player)
-}
-
-func (a *Api) updatePlayer(c *gin.Context) {
-	id := c.Param("id")
-	if id == "" {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "missing required parameter id"})
-		return
-	}
-	var json Player
-	err := c.ShouldBindJSON(&json)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-	var player Player
-	err = a.db.NewSelect().Model(&player).Scan(a.ctx)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-	if json.Name != "" {
-		player.Name = json.Name
-	}
-	_, err = a.db.NewUpdate().Model(&player).WherePK().Exec(a.ctx)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-	c.JSON(http.StatusOK, player)
-}
-
-func (a *Api) deletePlayer(c *gin.Context) {
-	inputId := c.Param("id")
-	if inputId == "" {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "missing required parameter id"})
-		return
-	}
-	id, err := strconv.Atoi(inputId)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid id given"})
-		return
-	}
-	player := Player{ID: id}
-	_, err = a.db.NewDelete().Model(&player).WherePK().Exec(a.ctx)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-	c.JSON(http.StatusNoContent, gin.H{})
-}
-
-func (a *Api) getPlayDates(c *gin.Context) {
-	var playdates []PlayDate
-	err := a.db.NewSelect().Model(&playdates).Scan(a.ctx)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-	c.JSON(http.StatusOK, playdates)
-}
-
-func (a *Api) createPlayDate(c *gin.Context) {
-	var json PlayDate
-	err := c.ShouldBindJSON(&json)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-	_, err = a.db.NewInsert().Model(&json).Exec(a.ctx)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-	c.JSON(http.StatusCreated, gin.H{})
-}
-
-func (a *Api) getPlayDate(c *gin.Context) {
-	id := c.Param("id")
-	if id == "" {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "missing required parameter id"})
-		return
-	}
-	var playdate PlayDate
-	err := a.db.NewSelect().Model(&playdate).Scan(a.ctx)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-	c.JSON(http.StatusOK, playdate)
-}
-
-func (a *Api) updatePlayDate(c *gin.Context) {
-	id := c.Param("id")
-	if id == "" {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "missing required parameter id"})
-		return
-	}
-	var json PlayDate
-	err := c.ShouldBindJSON(&json)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-	var playdate PlayDate
-	err = a.db.NewSelect().Model(&playdate).Scan(a.ctx)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-	if json.Game != "" {
-		playdate.Game = json.Game
-	}
-	if !json.Date.IsZero() {
-		playdate.Date = json.Date
-	}
-	_, err = a.db.NewUpdate().Model(&playdate).WherePK().Exec(a.ctx)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-	c.JSON(http.StatusOK, playdate)
-}
-
-func (a *Api) deletePlayDate(c *gin.Context) {
-	id := c.Param("id")
-	if id == "" {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "missing required parameter id"})
-		return
-	}
-	_, err := a.db.NewDelete().Where("id = ?", id).Exec(a.ctx)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-	c.JSON(http.StatusNoContent, gin.H{})
 }
--- server.go
+++ server.go
@@ -4,45 +4,53 @@ import (
 	"database/sql"
 	"embed"
 	"fmt"
-	"log"
 	"os"
 	"uc181discord/games/bot/internal"
 
 	"github.com/pressly/goose/v3"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"github.com/uptrace/bun"
 	"github.com/uptrace/bun/dialect/pgdialect"
 	"github.com/uptrace/bun/driver/pgdriver"
+	"github.com/uptrace/bun/extra/bundebug"
 )
 
 //go:embed migrations/*
 var embedMigrations embed.FS
 
 func main() {
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout})
+
 	// note that this is path referenced from within the docker container
 	banner, err := os.ReadFile("banner.txt")
 	if err != nil {
-		log.Fatal("Failed to read banner text file. err=", err)
+		log.Err(err).Msg("Failed to read banner text file")
 	}
-	log.Println(string(banner))
+	fmt.Print(string(banner))
 
 	dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", internal.Config.PostgresUser, internal.Config.PostgresPassword, internal.Config.PostgresHost, internal.Config.PostgresPort, internal.Config.PostgresDatabase)
 	sqldb := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(dsn)))
 	db := bun.NewDB(sqldb, pgdialect.New())
 	defer db.Close()
+	db.AddQueryHook(bundebug.NewQueryHook(bundebug.WithVerbose(true)))
+	// init m2m relationships the bun way
+	internal.InitializeManyToManyRelationships(db)
 
 	// Check the connection
 	if err := db.Ping(); err != nil {
-		log.Fatalf("failed to connect to PostgreSQL. url=%s err=%v", dsn, err)
+		log.Err(err).Str("url", dsn).Msg("failed to connect to PostgreSQL")
 	}
-	log.Println("Connected to PostgreSQL successfully!")
+	log.Debug().Msg("Connected to PostgreSQL successfully!")
 
 	// run database migrations automatically
 	goose.SetBaseFS(embedMigrations)
 	if err := goose.SetDialect("postgres"); err != nil {
-		panic(err)
+		log.Panic().Err(err).Msg("failed to set dialect for goose migrations")
 	}
 	if err := goose.Up(sqldb, "migrations"); err != nil {
-		panic(err)
+		log.Panic().Err(err).Msg("failed to run goose migrations")
 	}
 
 	internal.StartAPI(db) // start webserver on subprocess
